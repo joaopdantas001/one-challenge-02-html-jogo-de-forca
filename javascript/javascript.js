be4f846898fff890cd8d34f5c1b94101dea9b90c
JoaoPaulo <109078725+joaopdantas001@users.noreply.github.com>
2023-01-30 21:38:34 -0300

    v2
    visual ok

---
+++ javascript/javascript.js
@@ -0,0 +1,21 @@
+int idUm = 1;
+int idDois = 2;
+int idTres = 3;
+String grupoUm = "Legume";
+String grupoDois = "Profissão";
+String grupoTres = "País";
+int numero_de_letrasUm = 6;
+int numero_de_letrasDois = 6;
+int numero_de_letrasTres = 6;
+String palavraUm = "BATATA";
+String palavraDois = "MEDICO";
+String palavraTres = "BRASIL";
+
+function pulaLinha{
+    document.write("<br>");
+}
+
+function tentativa(){
+    alert("oi!");
+}
+
