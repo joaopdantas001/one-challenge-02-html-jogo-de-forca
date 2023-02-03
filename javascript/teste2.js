efb09a135ac01f57967c6763446979beacda72b8
JoaoPaulo <109078725+joaopdantas001@users.noreply.github.com>
2023-02-03 16:00:48 -0300

    v4.3

---
+++ javascript/teste2.js
@@ -0,0 +1,18 @@
+const tentativasContador = 0;
+const tentativas = 6;
+const posicaoLetraEncontrada = [0,1,2,3,4];
+var palavraOculta = "panela;";
+var palavraTamanho = 6;
+var letraOculta = "a";
+var posicaoEncontrada;
+
+var i = 0;
+
+
+    posicaoEncontrada = palavraOculta.indexOf(letraOculta);
+    posicaoLetraEncontrada[i] = posicaoEncontrada;
+    i++;
+
+
+alert(posicaoLetraEncontrada[0,1,2,3,4]);
+
