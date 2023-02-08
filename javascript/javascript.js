54308a44a229d47f27e371e4dbe8fb6f98031233
JoaoPaulo <109078725+joaopdantas001@users.noreply.github.com>
2023-02-08 15:51:04 -0300

    v4.6
    refatorando

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,6 +9,12 @@ for(i = 0; i < tamanho; i++){
 } 
 
 function letrasEncontradas(quantidadeDeLetrasEncontradas,letra){
+    for(i = 0; i < tamanho; i++){
+        if(palavra[i] == letra){
+            palavraOculta[i] = letra;
+            quantidadeDeLetrasEncontradas++;  
+        } 
+    }
     console.log("foram achadas " + quantidadeDeLetrasEncontradas + " letras " + letra);
     if(quantidadeDeLetrasEncontradas == 0){
         tentativas--;
@@ -25,12 +31,6 @@ function buscarLetraNaPalavraOculta_a(){
     var letra = document.getElementById("a").value;
     var quantidadeDeLetrasEncontradas = 0;
 
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -39,12 +39,6 @@ function buscarLetraNaPalavraOculta_b(){
     var letra = document.getElementById("b").value;
     let quantidadeDeLetrasEncontradas = 0;
 
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -53,25 +47,13 @@ function buscarLetraNaPalavraOculta_c(){
     var letra = document.getElementById("c").value;
     let quantidadeDeLetrasEncontradas = 0;
 
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
 function buscarLetraNaPalavraOculta_d(){
     var letra = document.getElementById("d").value;
     let quantidadeDeLetrasEncontradas = 0;
-    
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
+
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -79,12 +61,6 @@ function buscarLetraNaPalavraOculta_e(){
     var letra = document.getElementById("e").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -92,12 +68,6 @@ function buscarLetraNaPalavraOculta_f(){
     var letra = document.getElementById("f").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -105,12 +75,6 @@ function buscarLetraNaPalavraOculta_g(){
     var letra = document.getElementById("g").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -118,38 +82,20 @@ function buscarLetraNaPalavraOculta_h(){
     var letra = document.getElementById("h").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
 function buscarLetraNaPalavraOculta_i(){
     var letra = document.getElementById("i").value;
     let quantidadeDeLetrasEncontradas = 0;
-    
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
+
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
 function buscarLetraNaPalavraOculta_j(){
     var letra = document.getElementById("j").value;
     let quantidadeDeLetrasEncontradas = 0;
-    
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
+
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -157,12 +103,6 @@ function buscarLetraNaPalavraOculta_k(){
     var letra = document.getElementById("k").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -170,12 +110,6 @@ function buscarLetraNaPalavraOculta_l(){
     var letra = document.getElementById("l").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -183,12 +117,6 @@ function buscarLetraNaPalavraOculta_m(){
     var letra = document.getElementById("m").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -196,12 +124,6 @@ function buscarLetraNaPalavraOculta_n(){
     var letra = document.getElementById("n").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -209,12 +131,6 @@ function buscarLetraNaPalavraOculta_o(){
     var letra = document.getElementById("o").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -222,12 +138,6 @@ function buscarLetraNaPalavraOculta_p(){
     var letra = document.getElementById("p").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -235,12 +145,6 @@ function buscarLetraNaPalavraOculta_q(){
     var letra = document.getElementById("q").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -248,12 +152,6 @@ function buscarLetraNaPalavraOculta_r(){
     var letra = document.getElementById("r").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -261,12 +159,6 @@ function buscarLetraNaPalavraOculta_s(){
     var letra = document.getElementById("s").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -274,12 +166,6 @@ function buscarLetraNaPalavraOculta_t(){
     var letra = document.getElementById("t").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -287,12 +173,6 @@ function buscarLetraNaPalavraOculta_u(){
     var letra = document.getElementById("u").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -300,12 +180,6 @@ function buscarLetraNaPalavraOculta_v(){
     var letra = document.getElementById("v").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -313,12 +187,6 @@ function buscarLetraNaPalavraOculta_x(){
     var letra = document.getElementById("x").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -326,12 +194,6 @@ function buscarLetraNaPalavraOculta_y(){
     var letra = document.getElementById("y").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
@@ -339,24 +201,12 @@ function buscarLetraNaPalavraOculta_z(){
     var letra = document.getElementById("z").value;
     let quantidadeDeLetrasEncontradas = 0;
     
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
 
 function buscarLetraNaPalavraOculta_ç(){
     var letra = document.getElementById("ç").value;
     let quantidadeDeLetrasEncontradas = 0;
-    
-    for(i = 0; i < tamanho; i++){
-        if(palavra[i] == letra){
-            palavraOculta[i] = letra;
-            quantidadeDeLetrasEncontradas++;  
-        } 
-    }
+
     letrasEncontradas(quantidadeDeLetrasEncontradas,letra);
 }
